761afbb1729692901a68b509d2ab75d999f2793c
Chris Holman <squid@sirsquidness.com>
2016-04-01 20:22:29 +1100

    fix referencing wrong cache names

--- buildcontainer.sh
+++ buildcontainer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t origin .
+docker build -t cache .
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /origin/data /origin/logs
 
-docker run -it -p 80:80 -v /steam:/origin/data -v /var/log/nginx:/origin/logs origin
+docker run -it --rm -p 80:80 -v /steam:/origin/data -v /var/log/nginx:/origin/logs cache
