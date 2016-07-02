5eb2ebad2ff3161d688ac7c7c62dd6cec9b1ba69
Chris Holman <squid@sirsquidness.com>
2016-07-02 21:12:16 +1000

    Fix some typos in volume mount argument

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /data/data /data/logs
 
-docker run -it --rm -p 80:80 -v /steam:/data/data -v /var/log/nginx:/data/logs cache
+docker run -d --restart=always -p 80:80 -v /data/data:/cache -v /data/logs:/var/log/nginx cache
